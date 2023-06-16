6fec3fa9770e43a703a90520c449574a33140f2e
diegoandcontroll <lukasxdp2@gmail.com>
2023-06-16 11:50:22 -0300

    fix: implement user logged navbar actions

--- src/components/form/signin/index.tsx
+++ src/components/form/signin/index.tsx
@@ -1,7 +1,21 @@
+'use client'
+import { signIn } from 'next-auth/react'
 import Link from 'next/link'
+import React, { useRef } from 'react'
 
 /* eslint-disable @next/next/no-img-element */
 const SignInComponent = () => {
+  const email = useRef('')
+  const pass = useRef('')
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    await signIn('credentials', {
+      email: email.current,
+      password: pass.current,
+      redirect: true,
+      callbackUrl: '/',
+    })
+  }
   return (
     <section className="h-screen">
       <div className="h-full">
@@ -15,7 +29,7 @@ const SignInComponent = () => {
           </div>
 
           <div className="mb-12 md:mb-0 md:w-8/12 lg:w-5/12 xl:w-5/12">
-            <form>
+            <form onSubmit={handleSubmit}>
               <h2 className="text-center font-semibold text-3xl pb-8">
                 Sign In
               </h2>
@@ -28,6 +42,7 @@ const SignInComponent = () => {
                   className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-100"
                   id="exampleFormControlInput2"
                   placeholder="Digite seu email"
+                  onChange={(e) => (email.current = e.target.value)}
                 />
                 <label className="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[2.15] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[1.15rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[1.15rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary peer-focus:top-[-10.50px] peer-focus:opacity-100 peer-first:opacity-0">
                   Email
@@ -43,6 +58,7 @@ const SignInComponent = () => {
                   className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-100"
                   id="exampleFormControlInput22"
                   placeholder="Digite sua senha"
+                  onChange={(e) => (pass.current = e.target.value)}
                 />
                 <label className="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[2.15] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[1.15rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[1.15rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary peer-focus:top-[-10.50px] peer-focus:opacity-100 peer-first:opacity-0">
                   Password
@@ -51,7 +67,7 @@ const SignInComponent = () => {
 
               <div className="text-center lg:text-left flex justify-center items-center gap-8 flex-col">
                 <button
-                  type="button"
+                  type="submit"
                   className="inline-block rounded bg-cyan-600 px-24 pb-2.5 pt-3 text-sm  uppercase leading-normal text-white font-bold shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
                   data-te-ripple-init
                   data-te-ripple-color="light"
